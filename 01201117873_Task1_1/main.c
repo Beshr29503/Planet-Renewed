7bb3ebc73e19f61a7553418750371170336fd3fc
Ibrahim Beshr <himabishr@gmail.com>
2023-09-02 16:54:01 +0200

    Added Task 1.1 to Branch Task-1

---
+++ 01201117873_Task1_1/main.c
@@ -0,0 +1,19 @@
+//Ibrahim Mohamed Nabil Beshr
+//01201117873_Task1_1.c
+//14-8-2023
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char Gru[] = "GRU";
+    //Display GRU in words
+    printf("%s\n",Gru);
+    //Display GRU in *
+    printf("*****\t****\t*    *\n");
+    printf("*    \t*   *\t*    *\n");
+    printf("*  **\t****\t*    *\n");
+    printf("*   *\t*   *\t*    *\n");
+    printf("*****\t*    *  ******");
+    return 0;
+}
